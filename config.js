e5d59b2d679d16c7fd9628050a35e105aff39f26
DJ-Baker <debib8ker@yahoo.com>
2017-10-06 13:45:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   consumer_key:         't5TJ0jgupwluZIW1vOuldFDVi',
   consumer_secret:      'SQSOyfKyBvbKDUVW0jsknTfoBKZJCslWN8bGlnrIqjS9lAV7Cw',
   access_token:         '916350977778778113-t1hvSuk2AiITdF1RVp2Ca7T8fXkvTLB',
-  access_token_secret:  '	yHdqKLTm7wt7CGZIpAzcNkqGpO6va9caB8EWk7i3ebWHP'
+  access_token_secret:  'yHdqKLTm7wt7CGZIpAzcNkqGpO6va9caB8EWk7i3ebWHP'
 }
