8f676ec3bca2babef12e3564be903532b1cbae96
DJ-Baker <debib8ker@yahoo.com>
2017-10-06 13:44:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         't5TJ0jgupwluZIW1vOuldFDVi',
+  consumer_secret:      'SQSOyfKyBvbKDUVW0jsknTfoBKZJCslWN8bGlnrIqjS9lAV7Cw',
+  access_token:         '916350977778778113-t1hvSuk2AiITdF1RVp2Ca7T8fXkvTLB',
+  access_token_secret:  '	yHdqKLTm7wt7CGZIpAzcNkqGpO6va9caB8EWk7i3ebWHP'
 }
